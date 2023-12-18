66f7248deeed6393bc32e8b46898868792299f80
Kayla <kehuber@students.ntc.edu>
2023-12-18 13:02:15 -0600

    Add files for proxy example

---
+++ ProxyPattern/DMVProxy.cs
@@ -0,0 +1,42 @@
+﻿using ObjectvilleDMV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+	public class DMVProxy : IDMV
+	{
+		private DMV realDMV;
+
+		public DMVProxy()
+		{
+
+		}
+		public void AddApplicant(Applicant a)
+		{
+			this.GetApplicants().Add(a);
+		}
+
+		public bool CheckAge(int age)
+		{
+			return age >= 16;
+		}
+
+		public void FillOutPaperwork()
+		{
+			Console.WriteLine("Filling out paperwork from home.");
+		}
+
+		public List<Applicant> GetApplicants()
+		{
+			if (realDMV == null)
+			{
+				this.realDMV = new DMV();
+			}
+			return realDMV.GetApplicants();
+		}
+	}
+}
---
+++ ProxyPattern/Program.cs
@@ -0,0 +1,32 @@
+﻿using ObjectvilleDMV;
+using System;
+using System.Collections.Generic;
+
+namespace ProxyPattern
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            IDMV dmvProxy = new DMVProxy();
+
+            if (dmvProxy.CheckAge(16))
+            {
+                Console.WriteLine("Applicant can apply for a license.");
+                dmvProxy.FillOutPaperwork();
+            }
+            else
+            {
+                Console.WriteLine("Applicant cannot apply for a license.");
+            }
+
+            Console.WriteLine();
+
+            List<Applicant> applicants = dmvProxy.GetApplicants();
+
+            applicants.ForEach(a => Console.WriteLine(a.ToString()));
+
+            Console.Read();
+        }
+    }
+}
